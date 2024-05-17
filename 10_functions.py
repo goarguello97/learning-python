5a43b66132f9e9726660a4993716a2658dfcb7d7
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2024-05-17 02:37:53 -0300

    I've even practiced the functions

---
+++ 10_functions.py
@@ -0,0 +1,52 @@
+### Functions ###
+
+
+def my_function():
+    print("Esto es una función")
+
+
+my_function()
+my_function()
+my_function()
+
+
+def sum_two_values(a, b):
+    print(a + b)
+
+
+sum_two_values(2, 10)
+sum_two_values("2", "10")
+sum_two_values(5.2, 6.8)
+
+
+def sum_two_values_with_return(a, b):
+    return a + b
+
+
+my_result = sum_two_values_with_return(4, 5)
+print(my_result)
+
+
+def print_name(name, surname):
+    print(f"{name} {surname}")
+
+
+print_name("Gonzalo", "Argüello")
+print_name(surname="Argüello", name="Gonzalo")
+
+
+def print_name_with_default(name, surname, alias="Sin alias"):
+    print(f"{name} {surname} {alias}")
+
+
+print_name_with_default("Gonzalo", "Argüello")
+print_name_with_default("Gonzalo", "Argüello", "GONZAARGUELLO")
+
+
+def print_text(*texts):
+    for text in texts:
+        print(text)
+
+
+print_text("Hola", "Python", "Test")
+print_text("Hola")
